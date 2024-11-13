d3ad895fdce5eef92be26e6bf294afaec3518ee6
ganhao <ganhao1687@gmail.com>
2024-11-13 13:45:53 +0800

    course schema

---
+++ server/models/cource-model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const courceSchema = new Schema({
+  id: { type: String },
+  title: {
+    type: String,
+    require: true,
+  },
+  decription: {
+    type: String,
+    require: true,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
+  instructor: {
+    type: mongoose.Schema.Types.ObjectId, //primary key
+    ref: "User",
+  },
+  students: {
+    type: [String],
+    default: [],
+  },
+});
+
+module.exports = mongoose.model("Cource", courceSchema);
